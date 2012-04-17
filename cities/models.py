83a055cb017b9ad465f75032d4c77d2b3a662910
Jared Morse <jarcoal@gmail.com>
2012-04-17 16:15:43 -0700

    created a unique constraint for name/region/country_code

--- cities/models.py
+++ cities/models.py
@@ -9,10 +9,9 @@ class City(models.Model):
 	Represents a City
 	"""
 	
-	name = models.CharField(max_length=255, db_index=True)
-	country_code = models.CharField(max_length=2, db_index=True)
-	
+	name = models.CharField(max_length=255, db_index=True)	
 	region = models.CharField(max_length=2, db_index=True)
+	country_code = models.CharField(max_length=2, db_index=True)
 	
 	latitude = models.FloatField(db_index=True)
 	longitude = models.FloatField(db_index=True)
@@ -36,3 +35,7 @@ class City(models.Model):
 	
 	def __unicode__(self):
 		return ', '.join([self.name, self.region, self.country_code])
+	
+	
+	class Meta:
+		unique_together = (('name', 'region', 'country_code'),)
